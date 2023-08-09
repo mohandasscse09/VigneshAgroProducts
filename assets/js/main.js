e9cc5fce4c72ddad0edf211a643aeef81cda0101
Mohandass <mohandasscse09@gmail.com>
2023-08-09 11:51:40 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -162,3 +162,64 @@ document.addEventListener("DOMContentLoaded", () => {
   }
   window.addEventListener("load", initSwiper);
 });
+
+/**
+ * Section Toogle
+ */
+
+function myFunction() {
+  var x = document.getElementById("section2_brand");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+var words = [
+    "Fertilizer in Agriculture",
+    "The best fertilizer for a piece of land is the footprints of its owner.",
+  ],
+  part,
+  i = 0,
+  offset = 0,
+  len = words.length,
+  forwards = true,
+  skip_count = 0,
+  skip_delay = 15,
+  speed = 70;
+var wordflick = function () {
+  setInterval(function () {
+    if (forwards) {
+      if (offset >= words[i].length) {
+        ++skip_count;
+        if (skip_count == skip_delay) {
+          forwards = false;
+          skip_count = 0;
+        }
+      }
+    } else {
+      if (offset == 0) {
+        forwards = true;
+        i++;
+        offset = 0;
+        if (i >= len) {
+          i = 0;
+        }
+      }
+    }
+    part = words[i].substr(0, offset);
+    if (skip_count == 0) {
+      if (forwards) {
+        offset++;
+      } else {
+        offset--;
+      }
+    }
+    $(".word").text(part);
+  }, speed);
+};
+
+$(document).ready(function () {
+  wordflick();
+});
